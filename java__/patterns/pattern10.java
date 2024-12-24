9ca976c090e749edea5278c772c5864212e26135
913122104163_SuriyaLakshmi <suriyalakshmi413@gmail.com>
2024-12-24 17:48:50 +0530

    Create pattern10.java

---
+++ java__/patterns/pattern10.java
@@ -0,0 +1,25 @@
+    // Input: ‘N’ = 3
+    
+    // Output: 
+    
+    // *
+    // **
+    // ***
+    // **
+    // *
+
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+
+        for(int i=1; i<2*n; i++){
+            int stars = i;
+            if(i>n)
+                stars = 2*n-i;
+
+            for(int j=1; j<=stars; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
