d6955503c6fad2e04f38d72b80add910ed1efbe8
913122104163_SuriyaLakshmi <suriyalakshmi413@gmail.com>
2024-12-24 17:51:11 +0530

    Create pattern11.java

---
+++ java__/patterns/pattern11.java
@@ -0,0 +1,26 @@
+    // Input: ‘N’ = 3
+    
+    // Output: 
+    
+    // 1
+    // 0 1
+    // 1 0 1
+
+public class Solution {
+    public static void nBinaryTriangle(int n) {
+        // Write your code here.
+
+        int start = 1;
+        for(int i=0; i<n; i++){
+            if(i%2==0)
+                start = 1;
+            else    
+                start = 0;
+            for(int j=0; j<=i; j++){
+                System.out.print(start+" ");
+                start = 1-start;
+            }
+            System.out.println();
+        }
+    }
+}
